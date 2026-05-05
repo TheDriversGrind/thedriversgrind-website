42b91bcfaaa9154d7776c8d8a1300c192418c75f
TheDriversGrind <rkas1414@gmail.com>
2026-05-05 13:03:48 -0400

    Add src/config.ts

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+    trackerUrl: "https://driver-grind-pro.replit.app",
+    youtubeUrl: "https://www.youtube.com/@TheDriversGrind26",
+    facebookUrl: "https://www.facebook.com/share/g/1DrzvXsgh9/",
+    etsyUrl: "https://www.etsy.com/shop/TheDriversGrind",
+    contactEmail: "mailto:contact@thedriversgrind.com",
+    emailUrl: "https://thedriversgrind-kj64oc.subscribepage.io",
+  };
